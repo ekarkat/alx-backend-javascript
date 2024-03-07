23954e916c257ac16d306482b294c31b332bfbdd
ekarkat <ekarkat@gmail.com>
2024-03-07 21:37:34 +0000

    Unexpected use of file extension "js" for "./0-classroom.js"

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const class0 = new ClassRoom(19);
+  const class1 = new ClassRoom(20);
+  const class2 = new ClassRoom(34);
+  return [class0, class1, class2];
+}
