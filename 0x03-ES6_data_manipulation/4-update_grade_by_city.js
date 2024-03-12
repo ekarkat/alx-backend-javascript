fce823aa8032c29095dcea94533b4cd4b1e105a4
ekarkat <ekarkat@gmail.com>
2024-03-12 23:24:30 +0000

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+  return listStudents
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+      let grade;
+      if (matchingGrade && matchingGrade.grade !== undefined) {
+        grade = matchingGrade.grade;
+      } else {
+        grade = 'N/A';
+      }
+      return { ...student, grade };
+    });
+}
