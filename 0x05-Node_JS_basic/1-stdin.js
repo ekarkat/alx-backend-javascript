fc005e3676d7f23967aa77c76375dd2ac3feb48e
ekarkat <ekarkat@gmail.com>
2024-05-01 02:48:07 +0100

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
-    console.log(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 process.stdin.on('end', () => {
