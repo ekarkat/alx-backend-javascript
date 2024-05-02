fff312793247e248175767b7fa304f82571e6b6b
ekarkat <ekarkat@gmail.com>
2024-05-02 20:57:52 +0100

    task 10 part 1

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,1 +1,28 @@
-#!/usr/bin/node
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.end('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
+});
+
+app.get('/available_payments', (req, res) => {
+  const options = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  };
+  res.json(options);
+});
+
+app.post('/login', (req, res) => {
+  res.end(`Welcome ${req.body.userName}`);
+});
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,1 +1,27 @@
-#!/usr/bin/node
+const request = require('request');
+const expect = require('chai').expect;
+
+describe('Index page', function () {
+  it('should resturn correct body and status code', (done) => {
+    request.get('http://localhost:7865', function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+	it('check the result  when id is a number', (done) => {
+    request.get('http://localhost:7865/cart/200', function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 200');
+      done();
+    });
+  });
+
+	it('check when id is not a number', (done) => {
+    request.get('http://localhost:7865/cart/a', function (error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
