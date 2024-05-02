7c2e9d22aeb61fb66f04a7eb78af719570a589af
ekarkat <ekarkat@gmail.com>
2024-05-02 17:02:09 +0100

    task 1 redo

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,22 +2,20 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', function () {
-  it('should return 0', function () {
-    assert.strictEqual(calculateNumber('SUM', -1.2, 1.4), 0);
-    assert.strictEqual(calculateNumber('SUM', 1.2, -1.5), 0);
+  it('should add two numbers', function () {
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
   });
 
-  it('should return 0', function () {
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 1.3), 0);
+  it('should subtract two numbers', function () {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
   });
 
-  it('should return division', function () {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.2), 0.25);
-    assert.strictEqual(calculateNumber('DIVIDE', -2.4, -3.6), 0.5);
+  it('should divide two numbers', function () {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
   });
 
-  it('should return Error', function () {
-    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0.4), 'Error');
+  it('should return Error when dividing by zero', function () {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
   });
 
 });
