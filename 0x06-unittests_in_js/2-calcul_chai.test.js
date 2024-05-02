9cb7ac7342d346c651b908e6eae67f86830ed9bd
ekarkat <ekarkat@gmail.com>
2024-05-02 17:05:04 +0100

    unitest task 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,19 +2,19 @@ const expect = require('chai').expect;
 const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', function () {
-  it('should add two numbers', function () {
+  it('should calculate sun', function () {
     expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
   });
 
-  it('should subtract two numbers', function () {
+  it('should calculate sub', function () {
     expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
   });
 
-  it('should divide two numbers', function () {
+  it('should calculate divide', function () {
     expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
   });
 
-  it('should return Error when dividing by zero', function () {
+  it('should return Error', function () {
     expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
   });
 
