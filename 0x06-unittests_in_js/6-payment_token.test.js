d227bb10c121fad1c5c92eb0ae4c34f66b3c3bdf
ekarkat <ekarkat@gmail.com>
2024-05-02 20:26:36 +0100

    task 6 and 7

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const getPaymentTokenFromAPI = require('./6-payment_token.js');
+const expect = require('chai').expect;
+
+describe('getPaymentTokenFromAPI', function () {
+  it('Async test', function (done) {
+    getPaymentTokenFromAPI(true)
+      .then((resolve) => {
+        expect(resolve.data).to.equal('Successful response from the API');
+        done();
+      })
+  });
+});
