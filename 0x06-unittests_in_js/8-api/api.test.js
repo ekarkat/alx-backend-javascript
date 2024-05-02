04d18c2279adf64c37047312dc9c8b7737a41d98
ekarkat <ekarkat@gmail.com>
2024-05-02 20:37:55 +0100

    ass the test cases

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const request = require('request');
+const expect = require('chai').expect;
+
+describe('Index page', function () {
+  it('should resturn correct body and status code', (done) => {
+    request.get('http://localhost:7865', function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
