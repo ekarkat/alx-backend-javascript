658bdae19602f7be336d7d066ea577b37ff5ae87
ekarkat <ekarkat@gmail.com>
2024-05-02 18:04:38 +0100

    task 5

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+const sendPaymentRequestToApi = require('./3-payment.js');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const Utils = require('./utils.js');
+
+describe('sendPaymentRequestToApi', function () {
+  let spyConsole;
+  beforeEach(() => spyConsole = sinon.spy(console, 'log'));
+  afterEach(() => spyConsole.restore());
+
+  it('sendPaymentRequestToAPI with 100, and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(spyConsole.calledWith('The total is: 120')).to.be.true;
+  });
+  it('sendPaymentRequestToAPI with 10, and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(spyConsole.calledWith('The total is: 20')).to.be.true;
+  });
+});
