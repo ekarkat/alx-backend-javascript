e67c27780d7e7b9e49c9f5d7dbee943c8bc0db34
ekarkat <ekarkat@gmail.com>
2024-05-04 01:51:48 +0100

    task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,10 +1,20 @@
 const http = require('http');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') res.write('Hello Holberton School!');
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
+    try {
+      const data = await countStudents(process.argv[2]);
+      for (const key in data) {
+        if (key) res.write(`Number of students in ${key}: ${data[key].length}. List: ${data[key].join(', ')}\n`);
+      }
+    } catch (error) {
+      res.end(error.message);
+    }
   }
+  res.end();
 });
 app.listen(1245);
 module.exports = app;
