36ff3daa77ac4c8d6fe17958a719ce3e409fa50d
ekarkat <ekarkat@gmail.com>
2024-05-04 01:23:48 +0100

    task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,1 +1,32 @@
-#!/usr/bin/node
+const fs = require('fs').promises;
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8')
+    .then((data) => {
+      // eslint-disable-next-line
+      let dataCopy = data.split('\n').slice(1);
+      // eslint-disable-next-line
+      let students = [];
+      for (const line of dataCopy) {
+        if (line) students.push(line);
+      }
+      console.log(`Number of students: ${students.length}`);
+      // eslint-disable-next-line
+      let fields = {};
+      for (const line of students) {
+        if (line.split(',')[3] in fields) {
+          fields[line.split(',')[3]].push(line.split(',')[0]);
+        } else {
+          fields[line.split(',')[3]] = [];
+          fields[line.split(',')[3]].push(line.split(',')[0]);
+        }
+      }
+      for (const key in fields) {
+        if (key) console.log(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
+      }
+      resolve();
+    }).catch(() => {
+      reject(new Error('Cannot load the database'));
+    });
+});
+module.exports = countStudents;
